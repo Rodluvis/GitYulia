e5f9f48046f1ac3eefefe00dc20d6de21cb47471
Yulia Rokotova <fastlagsgatan@gmail.com>
2020-11-18 11:18:44 +0100

    Add project files.

---
+++ GitYulia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace GitYulia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfNumbers = new int[5];
+            arrayOfNumbers[0] = 13;
+            arrayOfNumbers[1] = 22;
+            arrayOfNumbers[2] = 13;
+            arrayOfNumbers[3] = 22;
+            arrayOfNumbers[4] = 13;
+            arrayOfNumbers[5] = 22;
+
+            Console.WriteLine("Check if you pick a lucky number");
+            int myNr = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i <arrayOfNumbers.Length; i++)
+            {
+                if (myNr == arrayOfNumbers[i])
+                {
+                    Console.WriteLine("Winner wineer");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not this time");
+                }
+            }
+            Console.WriteLine(arrayOfNumbers.Contains(3));
+    }
+}
